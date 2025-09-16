156eeba07cef548d8ce00bb4c0ee59aefd5ea708
aymenkh9090 <aymen.bouraoui7@gmail.com>
2025-09-16 22:44:39 +0100

    euraka discovery

---
+++ microservices/config-server/src/test/java/tn/aymen/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.aymen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
